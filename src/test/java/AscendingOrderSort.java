25053c940d0fe51c1b0e07c81bea2f047d7b01e4
Divya123450 <109574975+Divya123450@users.noreply.github.com>
2024-08-09 00:51:52 +0530

    My First GitHub Project Pushing

---
+++ src/test/java/AscendingOrderSort.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class AscendingOrderSort {
+	
+
+	 public static void main(String[] args) {
+	        // Sample array
+	        int[] arrayToSort = {5, 2, 8, 1, 6};
+
+	        // Sorting the array in ascending order
+	        Arrays.sort(arrayToSort);
+
+	        // Displaying the sorted array
+	        System.out.println("Sorted Array in Ascending Order: " + Arrays.toString(arrayToSort));
+	        //System.out.println(Arrays.toString(arrayToSort));//Another way
+	    }
+	}
+
+
+
